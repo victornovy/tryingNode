cf1f91dab7eb43fc37d60fce14f23b3ff7a3fe3b
Victor Novy <novyvictor@gmail.com>
2016-04-10 17:30:46 -0300

    working with callback

--- rectangle/simpleNodeRect.js
+++ rectangle/simpleNodeRect.js
@@ -5,3 +5,21 @@ exports.perimeter = function(b, l) {
 exports.area = function(b, l) {
     return b*l;
 }
+
+module.exports = function(b, l, callback) {
+    try {
+        if (b < 0 || l < 0)
+            throw new Error("You can't calculate the area or perimeter with negative values");
+
+        callback(null, {
+            perimeter: function() {
+                return 2*(b+l);
+            },
+            area: function() {
+                return b*l;
+            }
+        });
+    } catch(error) {
+        return callback(error, null)
+    }
+}
--- rectangle/simpleNodeSolve.js
+++ rectangle/simpleNodeSolve.js
@@ -1,13 +1,24 @@
 var rect = require('./simpleNodeRect');
 
 function solveRect(b, l) {
+    /*
     if (b < 0 || l < 0)
         return console.log("You can't calculate the area or perimeter with negative values");
 
-    console.log("================AREA================");
-    console.log(rect.area(b, l));
-    console.log("=============PERIMETER==============");
-    console.log(rect.perimeter(b, l));
+    console.log("  =============================");
+    console.log("||PERIMETER: " + rect.area(b, l));
+    console.log("||     AREA: " + rect.perimeter(b, l));
+    console.log("  =============================");
+    */
+    rect(b, l, function(error, callback) {
+        if (!!error)
+            return console.log(error);
+
+        console.log("  =============================");
+        console.log("||PERIMETER: " + callback.perimeter());
+        console.log("||     AREA: " + callback.area());
+        console.log("  =============================");
+    });
 }
 
 solveRect(5, 5);
