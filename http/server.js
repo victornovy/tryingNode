c6563b16122d895033facc492e606579b0c8223b
Victor Novy <novyvictor@gmail.com>
2016-04-11 23:41:29 -0300

    reading files and writing heads with node service

--- http/server.js
+++ http/server.js
@@ -1,13 +1,44 @@
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
 var hostname = 'localhost';
 var port = 3000;
 
 var server = http.createServer(function(req, res) {
-    console.log(req.headers);
 
-    res.writeHead(200, {'Context-Type': 'text/html'});
-    res.end('<h1>HELLO</h1>');
+    console.log('Request for '+ req.url + ' by method ' + req.method);
+
+    if (req.method === 'GET') {
+        var fileUrl;
+
+        if (req.url === '/')
+            fileUrl = '/index.html';
+        else
+            fileUrl = req.url;
+
+        var filePath = path.resolve('./public' + fileUrl);
+        var fileExt = path.extname(filePath);
+
+        if (fileExt === '.html') {
+            fs.exists(filePath, function(exists) {
+                if (!exists) {
+                    res.writeHead(404, {'Context-Type': 'text/html'});
+                    res.end('<h1>ERROR 404: ' + fileUrl + ' not found</h1>');
+                    return;
+                }
+
+                res.writeHead(200, {'Context-Type': 'text/html'});
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } else {
+            res.writeHead(404, {'Context-Type': 'text/html'});
+            res.end('<h1>ERROR 404: ' + fileUrl + ' not a HTML file</h1>');
+        }
+    } else {
+            res.writeHead(404, {'Context-Type': 'text/html'});
+            res.end('<h1>ERROR 404: ' + req.method + ' not supported</h1>');
+    }
 });
 
 server.listen(port, hostname, function() {
