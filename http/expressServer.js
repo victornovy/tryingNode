a04ffc3ebe401f901c3fd6d46b72347cf1d63626
Victor Novy <novyvictor@gmail.com>
2016-04-14 23:42:25 -0300

    -another way to call a server supporting rest

--- http/expressServer.js
+++ http/expressServer.js
@@ -7,37 +7,45 @@ var express = require('express'),
 var app = express();
 
 app.use(morgan('dev'));
-app.use(bodyParser.json());
 
-app.all('/dishes', function(req, res, next) {
-    res.writeHead(200, {'Context-Type': 'text/plain'});
-    next();
-});
+var dishRouter = express.Router();
 
-app.get('/dishes', function(req, res, next) {
-    res.end('Will send all the dishes to you.');
-});
+dishRouter.use(bodyParser.json());
 
-app.post('/dishes', function(req, res, next) {
-    res.end('Will and the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
+dishRouter.route('/')
+    .all(function(req, res, next) {
+        res.writeHead(200, {'Context-Type': 'text/plain'});
+        next();
+    })
+    .get(function(req, res, next) {
+        res.end('Will send all the dishes to you.');
+    })
+    .post(function(req, res, next) {
+        res.end('Will and the dish: ' + req.body.name + ' with details: ' + req.body.description);
+    })
+    .delete(function(req, res, next) {
+        res.end('Delete all dishes');
+    })
+;
 
-app.delete('/dishes', function(req, res, next) {
-    res.end('Delete all dishes');
-});
+dishRouter.route('/:dishId')
+    .all(function(req, res, next) {
+        res.writeHead(200, {'Context-Type': 'text/plain'});
+        next();
+    })
+    .get(function(req, res, next) {
+        res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+    })
+    .put(function(req, res, next) {
+        res.write('Updating the dish: ' + req.params.dishId + '\n');
+        res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+    })
+    .delete(function(req, res, next) {
+        res.end('Deleting dishe: ' + req.params.dishId);
+    })
+;
 
-app.get('/dishes/:dishId', function(req, res, next) {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-});
-
-app.put('/dishes/:dishId', function(req, res, next) {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', function(req, res, next) {
-    res.end('Deleting dishe: ' + req.params.dishId);
-});
+app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname + '/public'));
 
